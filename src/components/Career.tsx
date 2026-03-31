40e4a1ca1d6face47844d7215fe3aeef9c9867b0
Singh007 <thakurak9415@gmail.com>
2026-03-31 13:44:17 -0400

    Revise career information and add CodeChef role
    Updated career details including roles, institutions, and timelines. Added a new section for CodeChef Club Leader role.

--- src/components/Career.tsx
+++ src/components/Career.tsx
@@ -15,25 +15,25 @@ const Career = () => {
           <div className="career-info-box">
             <div className="career-info-in">
               <div className="career-role">
-                <h4>M.Sc. Researcher & Intern</h4>
-                <h5>INRS, Canada</h5>
+                <h4>M.Sc. in Water Science</h4>
+                <h5>INRS, Québec, Canada</h5>
               </div>
-              <h3>2023-NOW</h3>
+              <h3>2024-NOW</h3>
             </div>
             <p>
-              Leading thesis on Bias Correction of Ensemble Environmental Forecasts using Deep Learning (LSTM). Previously contributed to the research project "Non-stationary Modelling of Wind Speed" managing large environmental datasets.
+              Focusing on numerical modeling, machine learning, and scientific data analysis. Leading thesis on Bias Correction of Ensemble Environmental Forecasts using Deep Learning (LSTM).
             </p>
           </div>
           <div className="career-info-box">
             <div className="career-info-in">
               <div className="career-role">
-                <h4>CodeChef Club Leader</h4>
-                <h5>SRMIST, India</h5>
+                <h4>Research Intern</h4>
+                <h5>INRS, Canada</h5>
               </div>
-              <h3>2019-23</h3>
+              <h3>2023</h3>
             </div>
             <p>
-              Managed a core team of 50 members and mentored a community group of over 1000 members, helping them in Competitive Programming and Software Development.
+              Contributed to the research project "Non-stationary Modelling of Wind Speed" managing large datasets and analyzing results. Awarded the MITACS Globalink Research Internship.
             </p>
           </div>
           <div className="career-info-box">
@@ -46,6 +46,18 @@ const Career = () => {
             </div>
             <p>
               Maintained and updated client websites, including content updates and functional improvements. Enhanced website usability and performance using WordPress plugins and configuration tools.
+            </p>
+          </div>
+          <div className="career-info-box">
+            <div className="career-info-in">
+              <div className="career-role">
+                <h4>CodeChef Club Leader</h4>
+                <h5>SRMIST, India</h5>
+              </div>
+              <h3>2019-23</h3>
+            </div>
+            <p>
+              Managed a core team of 50 members and mentored a community group of over 1000 members, helping them in Competitive Programming and Software Development.
             </p>
           </div>
         </div>
